abb8352136e31ea7827c10819d547f5762f80475
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-17 21:37:01 -0700

    started to write a few tests

--- spec/application_controller_spec.rb
+++ spec/application_controller_spec.rb
@@ -11,3 +11,22 @@ describe ApplicationController do
     expect(last_response.body).to include("Welcome to the Sinatra Template!")
   end
 end
+
+describe "Signup Page" do
+
+	it 'loads the signup page' do
+	  get '/signup'
+	  expect(last_response.status).to eq(200)
+	end
+
+it 'signup directs user to shipper-compliance index' do
+      params = {
+        :username => "nelsonmuntz",
+        :email => "haha@juno.com",
+        :password => "nukethewales"
+      }
+      post '/signup', params
+      expect(last_response.location).to include("/companies")
+    end
+
+end 
