c57f7990bf3922f3e9c8cc31f37a6fbddb063731
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-19 21:36:15 -0700

    created users controller and added signup and login routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+
+  get '/signup' do 
+     erb :"users/signup"  
+		
+  end
+
+  post '/login' do
+  	@user = User.find_by(username: params[:username])
+  end
+
+end
