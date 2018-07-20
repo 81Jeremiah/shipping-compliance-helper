dde7680434bfa3c277caa9962b20a84a66881b14
81Jeremiah <jeremiahlutes@gmail.com>
2018-07-20 08:55:05 -0700

    added post route or signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,16 @@ class UsersController < ApplicationController
 
   post '/login' do
   	@user = User.find_by(username: params[:username])
+  	redirect to "/companies"
   end
-
-
+  
+  post "/signup" do
+  	@user = User.create(params)
+  	if @user.save && !params[:username].empty? && !params[:email].empty? #only allows new user if user name and email are entered
+  	session[:user_id] = @user.id	
+    redirect to "/companies"
+    else
+    redirect to "/signup"
+    #flas message please enter email username & password to continue   
 
 end
