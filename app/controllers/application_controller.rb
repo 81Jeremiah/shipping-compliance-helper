588adc7318f751192d79582e15dfd042b6cf550c
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-21 12:12:01 -0700

    add sinatra-flash and flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,22 @@
 require './config/environment'
+require 'sinatra/base'
+require 'sinatra/flash'
 
 class ApplicationController < Sinatra::Base
+
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
     set :session_secret, 'secret_password'
+    register Sinatra::Flash
+
   end
 
   get "/" do
     erb :index
+
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,21 @@ class UsersController < ApplicationController
   
   post "/signup" do
   	@user = User.create(params)
-  	if @user.save && !params[:username].empty? && !params[:email].empty? #only allows new user if user name and email are entered
+  	if @user.save && !params[:username].empty? && !params[:email].empty? && !params[:password].empty? #only allows new user if user name and email are entered
     	session[:user_id] = @user.id	
       redirect to "/companies"
-      else
+    elsif params[:email].empty?
+      flash[:no_email_warning] = "You Must Enter an Email to Continue."
       redirect to "/signup"
-    #flas message please enter email username & password to continue   
+    elsif params[:username].empty?
+      flash[:no_username_warning] = "please enter a username to continue"
+      redirect to "/signup"
+    elsif params[:password].empty?
+      flash[:no_password_warning] = "please enter a password to continue"
+      redirect to "/signup"
+    else
+      redirect to "/signup"
+    #flash message please enter email username & password to continue   
     end
   end
 end
