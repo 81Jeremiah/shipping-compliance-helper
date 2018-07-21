ea864927d1bb8d6dcc8905d6c3f06b1d6b4528a6
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-21 13:04:19 -0700

    added validations for username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@ class User < ActiveRecord::Base
 	has_many :comments
 
 	has_secure_password
+
+	validates_presence_of :username, :email
+	validates :username, uniqueness: true
+	validates :email, uniqueness: true
+
 	
 end
