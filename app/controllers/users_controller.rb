a4a9b664c282905e3ea352440d416117aeb2db7b
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-21 13:22:25 -0700

    added email uniqueness flash warning

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,15 +15,19 @@ class UsersController < ApplicationController
   	if @user.save && !params[:username].empty? && !params[:email].empty? && !params[:password].empty? #only allows new user if user name and email are entered
     	session[:user_id] = @user.id	
       redirect to "/companies"
-    elsif params[:email].empty?
-      flash[:no_email_warning] = "You Must Enter an Email to Continue."
-      redirect to "/signup"
     elsif params[:username].empty?
       flash[:no_username_warning] = "You Must Enter an Username to Continue."
       redirect to "/signup"
     elsif User.all.any?{|user|user.username == params[:username]}
       flash[:username_warning] = "Sorry that username is already taken"
       redirect to "/signup"
+    elsif params[:email].empty?
+      flash[:no_email_warning] = "You Must Enter an Email to Continue."
+      redirect to "/signup"
+    elsif User.all.any?{|user|user.email == params[:email]}
+      flash[:username_warning] = "That email already has an account, did you mean to login?"
+      redirect to "/signup"
+    
     elsif params[:password].empty?
       flash[:no_password_warning] = "You Must Enter an Password to Continue."
       redirect to "/signup"
