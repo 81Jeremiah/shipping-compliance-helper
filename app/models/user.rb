3863999c4e40a02edf650f0e579bf7fe56c9bbce
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-22 15:39:39 -0700

    added validators to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ActiveRecord::Base
+	validates_presence_of :username, :email
+	validates :username, uniqueness: true
+	validates :email, uniqueness: true
+
 	has_many :companies
 	has_many :comments
 
 	has_secure_password
 
-	validates_presence_of :username, :email
-	validates :username, uniqueness: true
-	validates :email, uniqueness: true
+	
     
     def slug
       self.username.split(" ").join("-").downcase
