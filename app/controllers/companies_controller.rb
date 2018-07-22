7ee288db9f3734139c3a9f2f371b9c20087694d5
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-21 21:26:53 -0700

    added login verification to show new company form

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -15,19 +15,23 @@ class CompaniesController < ApplicationController
 	  	@user = User.find_by(id: session[:user_id])
 	  	erb :'companies/new'
 	  else
-	  	flash[:not_logged_in] = "You Must login First."
+	  	flash[:not_logged_in] = "You must login to view that page."
 		redirect to "/"
 	  end
     end
 
     post '/companies/:userslug' do
-      if !params[:name].empty?
+      
+
+      if !params[:name].empty? && Company.all.none?{|company|company.name == params[:name]}
       	@user = User.find_by_slug(params[:userslug])
       	@company = Company.create(name: params[:name], shipping_container_notes: params[:shipping_container_notes], label_notes: params[:label_notes], asn_notes: params[:asn_notes], routing_notes: params[:routing_notes], user_id: @user.id )
         #binding.pry
         redirect to "/companies/#{@company.id}"
+      
+
        else
-      	flash[:need_name] = "A company must have a name"
+      	flash[:need_name] = "A company must have a name and can't already be in the database."
       	redirect to "/companies/new"
        end
     end
