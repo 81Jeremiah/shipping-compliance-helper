5597436cdc4f7ab9cb4c989add83a9e4a645b248
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-21 23:44:54 -0700

    fixed comment controller spec to add comments

--- spec/controllers/04_comments_controller_spec.rb
+++ spec/controllers/04_comments_controller_spec.rb
@@ -6,16 +6,28 @@ describe CommentsController do
 
   describe "add comments to existing company" do
     it 'allows a user to add comments to an existing company if logged in' do
-      user = User.create(:username => "nelsonmuntz", :email => "haha@juno.com", :password => "nukethewales")
-      company1 = Company.create(:name => "Bed Bath", :user_id => user.id)
-      params = {
-        :username => "nelsonmuntz",
-        :password => "nukethewales",
-        :name => "Bed Bath",
-        :comment => "max box weight is no 50 lbs"
-      }
-      post '/comments', params
-      get '/companies/1'
+      user = User.create(:username => "milhouse", :email => "mymomsaysimcool@yahoo.com", :password => "imadud")
+      company = Company.create(:name => "Bed Bath", :user_id => user.id)
+
+      visit '/'
+
+        fill_in(:username, :with => "nelsonmuntz")
+        fill_in(:password, :with => "nukethewales")
+        click_button 'login'
+      
+      visit "/companies/#{company.id}"
+      fill_in(:user_comment, :with => "max box weight is no 50 lbs")
+      click_button 'submit'
+
+      # params = {
+      #   :username => "nelsonmuntz",
+      #   :password => "nukethewales",
+      #   :name => "Bed Bath",
+      #   :comment => "max box weight is no 50 lbs"
+      # }
+
+      #post '/comments/', params
+      get "/companies/#{company.id}"
       expect(last_response.body).to include("max box weight is no 50 lbs")
     end
   end
