702cbf4482af90de23a907fa914d09bd245c7c08
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-21 23:20:59 -0700

    created comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,11 @@
 class CommentsController < ApplicationController
 
+  post '/comments/:company_id' do
+  	 @user = current_user
+  	 @company = Company.find_by(id: params[:company_id])
+  	 @comment = Comment.create(user_comment: params[:user_comment], user_id: @user.id, company_id: @company.id)
+
+  	 redirect to "/companies/#{@company.id}"
+
+  end
 end
