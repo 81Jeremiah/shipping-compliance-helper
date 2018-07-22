90ff135e2ef63dd0e2c17bb016c9fb6eb6c691ae
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-21 18:47:36 -0700

    added slug method

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,13 @@ class User < ActiveRecord::Base
 	validates_presence_of :username, :email
 	validates :username, uniqueness: true
 	validates :email, uniqueness: true
-
+    
+    def slug
+      self.username.split(" ").join("-").downcase
+    end
+    
+    def self.find_by_slug(slug)
+      self.all.find{|user| user.slug == slug}
+    end
 	
 end
