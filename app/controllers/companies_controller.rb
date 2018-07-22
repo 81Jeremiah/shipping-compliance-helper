cf030bf2c92a1894a5bc3963f83a8d6cf427f802
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-21 18:24:59 -0700

    prevent people from going to company index without logging in

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,9 +1,13 @@
 class CompaniesController < ApplicationController
 
 	get '/companies' do 
-       @companies = Company.all
-       erb :'companies/index'
-		
+	  if logged_in?
+	    @companies = Company.all
+	    erb :'companies/index'
+	  else
+	  	flash[:nor_logged_in] = "You Must login First."
+		redirect to "/"
+	  end
 	end
 
 
