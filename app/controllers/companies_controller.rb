626c54e75590a66761ecacfb141159e0201fb0eb
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-21 20:35:32 -0700

    added companies/user and companies id routes

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -20,4 +20,25 @@ class CompaniesController < ApplicationController
 	  end
     end
 
+    post '/companies/:userslug' do
+      if !params[:name].empty?
+      	@user = User.find_by_slug(params[:userslug])
+      	@company = Company.create(name: params[:name], shipping_container_notes: params[:shipping_container_notes], label_notes: params[:label_notes], asn_notes: params[:asn_notes], routing_notes: params[:routing_notes], user_id: @user.id )
+        #binding.pry
+        redirect to "/companies/#{@company.id}"
+       else
+      	flash[:need_name] = "A company must have a name"
+      	redirect to "/companies/new"
+       end
+    end
+
+    get '/companies/:id' do 
+      @company = Company.find_by(id: params[:id])
+      if @company.user_id == session[:user_id]
+        erb :'/companies/show_company'
+      else
+       redirect to "/companies"
+      end
+    end
+
 end
