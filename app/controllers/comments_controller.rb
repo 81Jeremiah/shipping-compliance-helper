03bba30d34aee23d9f9f6834d7f34a4d15b59eb6
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-22 15:41:19 -0700

    added flash message to correspnd with new comment validator

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,14 @@ class CommentsController < ApplicationController
   	 @user = current_user
   	 @company = Company.find_by(id: params[:company_id])
   	 @comment = Comment.create(user_comment: params[:user_comment], user_id: @user.id, company_id: @company.id)
+     if @company.save
 
-  	 redirect to "/companies/#{@company.id}"
+  	   redirect to "/companies/#{@company.id}"
+  	 else
+  	 	flash[:comment_warning] = "Comment cannot be empty."
+
+  	 	redirect to "/companies/#{@company.id}"
+  	 end
 
   end
 end
