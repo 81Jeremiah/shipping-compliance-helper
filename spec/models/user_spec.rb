fb482ec1dd1e9768ea72412c35f8eae0b2fa7baa
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-24 21:40:12 -0700

    fixed spelling error in user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ describe 'User' do
     @user = User.create(:username => "william henry harrison", :email => "idiedin30days@aol.com", :password => "coatsr4wimps")
   end
   it 'can slug the username' do
-    expect(@user.slug).to eq("Willim-henry-harrison")
+    expect(@user.slug).to eq("william-henry-harrison")
   end
 
   it 'can find a user based on the slug' do
