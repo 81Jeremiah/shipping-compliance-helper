f13bc9a431d463f500c84fe05dd20344e9433bd6
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-24 21:41:29 -0700

    fixed application controller spec to match greeting

--- spec/controllers/01_application_controller_spec.rb
+++ spec/controllers/01_application_controller_spec.rb
@@ -11,7 +11,7 @@ describe ApplicationController do
     it 'loads the homepage' do
       get '/'
       expect(last_response.status).to eq(200)
-      expect(last_response.body).to include("Welcome, please log in")
+      expect(last_response.body).to include("Welcome!")
     end
   end
 end 
