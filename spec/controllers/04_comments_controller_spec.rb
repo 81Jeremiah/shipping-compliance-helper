1f884de4f5f7f8f26fc300e33c5bd01dc07ae6d1
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-24 21:40:59 -0700

    correct comment test to search page

--- spec/controllers/04_comments_controller_spec.rb
+++ spec/controllers/04_comments_controller_spec.rb
@@ -6,7 +6,7 @@ describe CommentsController do
 
   describe "add comments to existing company" do
     it 'allows a user to add comments to an existing company if logged in' do
-      user = User.create(:username => "milhouse", :email => "mymomsaysimcool@yahoo.com", :password => "imadud")
+      user = User.create(:username => "nelsonmuntz", :email => "mymomsaysimcool@yahoo.com", :password => "nukethewales")
       company = Company.create(:name => "Bed Bath", :user_id => user.id)
 
       visit '/'
@@ -16,19 +16,18 @@ describe CommentsController do
         click_button 'login'
       
       visit "/companies/#{company.id}"
-      fill_in(:user_comment, :with => "max box weight is no 50 lbs")
+      fill_in(:user_comment, :with => "max box weight is 50 lbs")
       click_button 'submit'
 
-      # params = {
-      #   :username => "nelsonmuntz",
-      #   :password => "nukethewales",
-      #   :name => "Bed Bath",
-      #   :comment => "max box weight is no 50 lbs"
-      # }
+       params = {
+        :user_comment => "max box weight is no 50 lbs",
+        :user_id => "#{user.id}",
+        :company_id => "#{company.id}"
+        
+      }
 
-      #post '/comments/', params
-      get "/companies/#{company.id}"
-      expect(last_response.body).to include("max box weight is no 50 lbs")
+      post "/comments/#{company.id}", params
+      expect(last_response.body).to include("max box weight is 50 lbs")
     end
   end
 end
