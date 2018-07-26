d9277e79a23d21423deca4a6a5bd9c935d8f4642
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-26 15:59:02 +0000

    refactored comment controller to remove dup code concepts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,19 @@
 class CommentsController < ApplicationController
 
   post '/comments/:company_id' do
-  	 @user = current_user
-  	 @company = Company.find_by(id: params[:company_id])
-  	 @comment = Comment.create(user_comment: params[:user_comment], user_id: @user.id, company_id: @company.id)
-     if @company.save
-  	   redirect to "/companies/#{@company.id}"
+  	 company = Company.find_by(id: params[:company_id])
+  	 comment = Comment.create(user_comment: params[:user_comment], user_id: current_user.id, company_id: company.id) # creates a comment that belongs to company & user
+     if comment.save
+  	   redirect to "/companies/#{company.id}"
   	 else
-  	 	 flash[:comment_warning] = "Comment cannot be empty."
-  	 	 redirect to "/companies/#{@company.id}"
+  	 	 redirect to "/companies/#{company.id}"
   	 end
   end
 
   get '/comments/:id/edit' do
-  	@comment = Comment.find_by(id: params[:id])
+  	@comment = Comment.find_by(id: params[:id]) # find comment to edit
   	@company = Company.find_by(id: @comment.company_id)
-  	if @comment.user_id = current_user.id
+  	if @comment.user_id = current_user.id #verify user own comment
   	  erb :'comments/edit_comment'
     else
       redirect to "/companies/#{@company.id}"
@@ -23,13 +21,13 @@ class CommentsController < ApplicationController
   end
 
   patch '/comments/:id' do
-    comment = Comment.find_by(id: params[:id])
-    company = Company.find_by(id: comment.company_id)
-    if !params[:user_comment].empty?
-      comment.update(user_comment: params[:user_comment])
+    comment = Comment.find_by(id: params[:id]) # find comment
+    company = Company.find_by(id: comment.company_id) #find company that belongs to comment
+
+    if  comment.update(user_comment: params[:user_comment])
       redirect to "/companies/#{comment.company_id}"
     else
-      redirect to "/comments/#{comment.id}/edit"
+      redirect to "/comments/#{comment.id}/edit" #if comment isn't valid redirects to edit form
     end
   end
 end
