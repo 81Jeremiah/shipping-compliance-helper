de45dcbe2408a079e3ed6cdb069ad4c516a8bcdb
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-28 20:11:38 -0700

    fixed redirects to company.slug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,9 +4,9 @@ class CommentsController < ApplicationController
   	 company = Company.find_by(id: params[:company_id])
   	 comment = Comment.create(user_comment: params[:user_comment], user_id: current_user.id, company_id: company.id) # creates a comment that belongs to company & user
      if comment.save
-  	   redirect to "/companies/#{company.id}"
+  	   redirect to "/companies/#{company.slug}"
   	 else
-  	 	 redirect to "/companies/#{company.id}"
+  	 	 redirect to "/companies/#{company.slug}"
   	 end
   end
 
@@ -16,7 +16,7 @@ class CommentsController < ApplicationController
   	if @comment.user_id = current_user.id #verify user own comment
   	  erb :'comments/edit_comment'
     else
-      redirect to "/companies/#{@company.id}"
+      redirect to "/companies/#{@company.slug}"
     end
   end
 
@@ -25,9 +25,16 @@ class CommentsController < ApplicationController
     company = Company.find_by(id: comment.company_id) #find company that belongs to comment
 
     if  comment.update(user_comment: params[:user_comment])
-      redirect to "/companies/#{comment.company_id}"
+      redirect to "/companies/#{company.slug}"
     else
-      redirect to "/comments/#{comment.id}/edit" #if comment isn't valid redirects to edit form
+      redirect to "/comments/#{comment.slug}/edit" #if comment isn't valid redirects to edit form
     end
+  end
+
+  delete '/comments/:id/delete' do #delete comment
+    comment = Comment.find_by(id: params[:id])
+    company = Company.find_by(id: comment.company_id)
+    comment.delete
+    redirect to "/companies/#{company.slug}"
   end
 end
